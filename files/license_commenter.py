972b0f06774434986a0a20504debf7943ea890b7
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-13 16:50:18 +0200

    finished line commenter

--- files/license_commenter.py
+++ files/license_commenter.py
@@ -4,6 +4,8 @@ import os
 Copyright 2017 Ruben Vorderman
 Copyright 2017 SASC Group (LUMC)
 '''
+
+
 def argument_parser():
     parser = argparse.ArgumentParser(prog='License Commenter',
                                      description='Parses a text file and returns a commented out license header')
@@ -14,34 +16,37 @@ def argument_parser():
     parser.add_argument('--overwrite', action='store_true')
     return parser.parse_args()
 
+
 def main():
     args = argument_parser()
 
-
     # Open input_file in read mode
     try:
-        with open(args.input,'r') as input_file =
-    except:
+        with open(args.input, 'r') as input_file:
+
+            # create output filename if does not exist
+            if not args.output:
+                args.output = os.path.splitext(os.path.basename(args.input))[0] + '.commented.txt'
+
+            # Check if output file can be written.
+            if os.path.exists(args.output):
+                if not args.overwrite:
+                    raise FileExistsError(args.output)
+                else:
+                    if os.path.isdir(args.output):
+                        raise IsADirectoryError(args.output)
+                    if os.path.isfile:
+                        os.remove(args.output)
+
+
+            # Write to output file
+            with open(args.output, 'w') as output_file:
+                for line in input_file.readlines():
+                    output_file.write(args.prefix + line.replace('\n','') + args.suffix + '\n')
+
+    except FileNotFoundError:
         raise FileNotFoundError(args.input)
 
-    # Open output file in write mode
-    # Check if output file already exists
-    if not args.output:
-        args.output = os.path.splitext(os.path.basename(args.input))[0] + '.commented.txt'
-    if os.path.exists(args.output):
-        if not args.overwrite:
-            raise FileExistsError(args.output)
-        else:
-            if os.path.isfile:
-                os.remove(args.output)
-            if os.path.isdir:
-                raise IsADirectoryError(args.output)
-    with open(args.output,'w') as output_file
 
-    for line in input_file.readlines():
-        output_file.write( args.prefix + line + args.suffix + '\n' )
-
-
-                os.
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
