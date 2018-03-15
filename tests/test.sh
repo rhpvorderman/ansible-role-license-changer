010be904bd798c448e9c86447251aadb3c88c519
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-03-15 13:36:44 +0100

    add project_root variable

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,4 @@
-ansible-playbook tests/test.yml -e "roles_path=$PWD/.."
-ansible-playbook tests/test.yml -e "roles_path=$PWD/.." -e "license_changer_check_mode=true"
+#!/usr/bin/env bash
+project_root=$(git rev-parse --show-toplevel)
+ansible-playbook tests/test.yml -e "roles_path=$project_root/.."
+ansible-playbook tests/test.yml -e "roles_path=$project_root/.." -e "license_changer_check_mode=true"
